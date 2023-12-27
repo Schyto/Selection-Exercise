65730b0edbdbba5179704bace3f110374fdbc995
Jaxon Panter <jmanpanter@gmail.com>
2023-12-27 13:57:16 -0600

    Final

--- Selection Exercise/Program.cs
+++ Selection Exercise/Program.cs
@@ -8,24 +8,27 @@ namespace SelectionStatementExercise
         {
             //Number Guessing Game
             var r = new Random();
-            var favNumber = r.Next(1, 1000);
-            
+            var favNumber = r.Next(0, 1001);
+            int userInput;
 
-            Console.WriteLine("Pick a number from 1 - 1000");
-            var userInput = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Pick a number from 1 - 1000");
+                userInput = int.Parse(Console.ReadLine());
 
-            if (userInput < favNumber)
-            {
-                Console.WriteLine($"{userInput} is too low");
-            }
-            else if (userInput > favNumber)
-            {
-                Console.WriteLine($"{userInput} is too high");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (userInput != favNumber);
         }
     }
 }
