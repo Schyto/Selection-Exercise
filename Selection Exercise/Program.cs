60e27baf907f74424d84c81e74f77b6a0b789653
Jaxon Panter <jmanpanter@gmail.com>
2023-12-27 13:58:41 -0600

    Final pt2

--- Selection Exercise/Program.cs
+++ Selection Exercise/Program.cs
@@ -29,6 +29,33 @@ namespace SelectionStatementExercise
                     Console.WriteLine("You guessed it!");
                 }
             } while (userInput != favNumber);
+            //Switch case
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower()) 
+            {
+                case "math":
+                case "calculus":
+                    Console.Writeline("I hate Math!");
+                    break;
+                case "science":
+                case "physics":
+                case "biology":
+                    Console.WriteLine("I love science!");
+                case "PE":
+                    Console.WriteLine("PE is the best!!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is fun,");
+                    break;
+                case "english":
+                    Console.WriteLine("English is boring.");
+                    break;
+                default:
+                    console.writeLine("I don't know that subject");
+            }
         }
     }
 }
